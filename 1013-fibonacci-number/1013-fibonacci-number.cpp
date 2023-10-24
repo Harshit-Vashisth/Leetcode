ddeaf6c8bf61eb474afba629c326111e872e9a5d
Harshit Vashisth <harshitvashisth76@gmail.com>
2023-10-24 17:14:56 +0530

    Time: 2 ms (77.87%), Space: 6.3 MB (28.06%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int solve(int n){
-        if(n==0 || n==1)
+    int recursion(int n){
+        if(n==0||n==1)
             return n;
-        vector<int> dp(n+1,-1);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
-            dp[i]=dp[i-2]+dp[i-1];
+        return (recursion(n-2)+recursion(n-1));
+    }
+    int topdown(int n,vector<int>& dp){
+        if(n==0||n==1)
+            return n;
+        if(dp[n]!=-1)
+        return dp[n];
+        dp[n]=topdown(n-2,dp)+topdown(n-1,dp);
         return dp[n];
     }
     int fib(int n) {
-        return solve(n);
+        vector<int> dp(n+1,-1);
+        return topdown(n,dp);
     }
 };
