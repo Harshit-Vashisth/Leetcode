ed0684d78742503c8946d508c5e3e077f8ba2464
Harshit Vashisth <harshitvashisth76@gmail.com>
2024-10-08 16:07:02 +0530

    Time: 10 ms (30.69%), Space: 10.4 MB (34.79%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,31 +8,34 @@
  */
 class Solution {
 public:
-    ListNode* detect(ListNode* head){
-         if(head==NULL)
-        return NULL;
-        ListNode* slow=head;
+    ListNode * detect(ListNode* head){
+        if(head==NULL||head->next==NULL)
+            return NULL;
         ListNode* fast=head;
-        while(fast!=NULL&&fast->next!=NULL&&slow->next!=NULL){
+        ListNode* slow=head;
+
+        while(fast!=NULL&&fast->next!=NULL&&slow!=NULL){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast)
-            return fast;
+            if(fast==slow)
+            return slow;
         }
         return NULL;
+    
     }
-    ListNode* cycle(ListNode* head,ListNode* det){
-         
-            while(head!=det&&det->next!=NULL){
-                head=head->next;
-                det=det->next;
-                }
-        return head;
+    ListNode* cycle(ListNode* head, ListNode* det){
+        ListNode* slow=head;
+        ListNode* fast=det;
+        while(slow!=fast&&slow->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        return slow;
     }
-      ListNode *detectCycle(ListNode *head) {
+        ListNode *detectCycle(ListNode *head) {
         if(head==NULL||head->next==NULL)
         return NULL;
-        ListNode *det=detect(head);
+        ListNode* det=detect(head);
         if(det==NULL)
         return NULL;
         return cycle(head,det);
