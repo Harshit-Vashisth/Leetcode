fe1ee0d15beeadaa3e93c6869f818810229651e0
Harshit Vashisth <harshitvashisth76@gmail.com>
2024-11-04 01:09:45 +0530

    Time: 3 ms (7.22%), Space: 8.8 MB (10.21%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length())
+        return false;
+        if(s==goal)
+        return true;
+        int l=goal.length();
+        while(l--){
+            if(s==goal)
+            return true;
+            s=s.substr(1)+s[0];
+        }
+        return false;
+    }
+};
