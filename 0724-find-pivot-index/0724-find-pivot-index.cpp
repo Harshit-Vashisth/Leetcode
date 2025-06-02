a1d4590989de25e80727525f858a7fc05bc053fa
Harshit Vashisth <harshitvashisth76@gmail.com>
2025-06-02 23:50:25 +0530

    Time: 1 ms (29.2%), Space: 35.6 MB (77.24%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& arr) {
+        int s1=0,s2=0;
+        for(int i =0;i<arr.size();i++){
+            s1+=arr[i];
+        }
+        for(int i =0;i<arr.size();i++){
+            s1-=arr[i];
+            if(s1==s2)
+            return i;
+            s2+=arr[i];
+        }
+          return -1;
+    }
+    
+};
