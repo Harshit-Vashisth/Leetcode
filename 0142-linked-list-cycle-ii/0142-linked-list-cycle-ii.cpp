2509be9349923c4068531be4cadd7437174c3d9b
Harshit Vashisth <harshitvashisth76@gmail.com>
2025-06-10 23:14:01 +0530

    Time: 10 ms (46.82%), Space: 11.5 MB (24.58%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -7,38 +7,43 @@
  * };
  */
 class Solution {
-public:
-    ListNode * detect(ListNode* head){
-        if(head==NULL||head->next==NULL)
-            return NULL;
+    /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+    ListNode* hasCycle(ListNode *head) {
         ListNode* fast=head;
         ListNode* slow=head;
-
-        while(fast!=NULL&&fast->next!=NULL&&slow!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(fast==slow)
+        while(fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+                slow=slow->next;
+                fast=fast->next;
+            }
+            if(slow==fast)
             return slow;
         }
         return NULL;
-    
     }
-    ListNode* cycle(ListNode* head, ListNode* det){
-        ListNode* slow=head;
-        ListNode* fast=det;
-        while(slow!=fast&&slow->next!=NULL){
-            fast=fast->next;
-            slow=slow->next;
-        }
-        return slow;
-    }
-        ListNode *detectCycle(ListNode *head) {
+
+public:
+    ListNode *detectCycle(ListNode *head) {
         if(head==NULL||head->next==NULL)
         return NULL;
-        ListNode* det=detect(head);
-        if(det==NULL)
+        ListNode* slow=head;
+        ListNode* fast=hasCycle(head);
+        if(fast==NULL)
         return NULL;
-        return cycle(head,det);
-
+        while(slow!=fast)
+        {
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return fast;
     }
 };
