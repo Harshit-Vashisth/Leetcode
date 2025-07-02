4c4390534dfb1fcdd2d75841b01cf9e16ef81bd2
Harshit Vashisth <harshitvashisth76@gmail.com>
2025-07-03 00:29:02 +0530

    Time: 0 ms (100%), Space: 10.9 MB (70.93%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -10,22 +10,18 @@
  * };
  */
 class Solution {
-    void order(TreeNode* root,vector<int> &ans){
-    if(!root)
-    return;
-   
-    order(root->left,ans);
-    order(root->right,ans);
-    ans.push_back(root->val);
-    
-}
 public:
+void solve(TreeNode* root, vector<int>& ans){
+        if(!root)
+        return;
+       
+        solve(root->left, ans);
+        solve(root->right, ans);
+        ans.push_back(root->val);
+    }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        if(root==NULL)
-            return ans;
-
-        order(root,ans);
+        solve(root,ans);
         return ans;
     }
 };
