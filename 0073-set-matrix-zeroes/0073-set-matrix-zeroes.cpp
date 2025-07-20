a4a5669f1d124b87eee8a61d46b2da4be3503661
Harshit Vashisth <harshitvashisth76@gmail.com>
2025-07-21 01:59:35 +0530

    Time: 3 ms (11.1%), Space: 18.5 MB (95.25%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    void rowise(vector<vector<int>>& matrix,int j){
-        for(int i=0;i<matrix.size();i++)
-            matrix[i][j]=0;
-    }
-    void colwise(vector<vector<int>>& matrix,int i){
-        for(int j=0;j<matrix[0].size();j++)
-            matrix[i][j]=0;
-    }
+   
     void setZeroes(vector<vector<int>>& matrix) {
-        set<pair<int,int>> st;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<pair<int,int>> arr;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==0)
-                {
-                   st.insert(make_pair(i,j));
+                if(matrix[i][j]==0){
+                   arr.push_back({i,j});
                 }
             }
         }
-        for(auto i : st){
-            rowise(matrix,i.second);
-            colwise(matrix,i.first);
+        for(auto [i,j]:arr){
+            for(int r=0;r<n;r++)
+                matrix[r][j]=0;
+            
+            for(int c=0;c<m;c++)
+                matrix[i][c]=0;
         }
     }
 };
