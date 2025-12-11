e2198b1c0ff5ecb1e0a3d0e9e2731d72ed9b3186
Harshit Vashisth <harshitvashisth76@gmail.com>
2025-12-12 01:54:47 +0530

    Time: 0 ms (100%), Space: 11.7 MB (84.41%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& str) {
+        string ans="";
+        for(int i =0;i<str[0].length();i++){
+            char ch=str[0][i];
+            bool match=true;
+            for(int j=1;j<str.size();j++){
+                 if(str[j].size()<i || ch!=str[j][i]){
+                    match=false;
+                    break;
+                 }
+            }
+            if(match==false){
+                break;
+            }
+            ans+=ch;
+        }
+        return ans;
+    }
+};
