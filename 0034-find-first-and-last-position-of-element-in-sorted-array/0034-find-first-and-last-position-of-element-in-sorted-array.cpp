1a88337febf6efe12aeb35ebd47e7701e95aea9e
Harshit Vashisth <harshitvashisth76@gmail.com>
2026-01-06 14:40:09 +0530

    Time: 0 ms (100%), Space: 17.7 MB (16.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
-int firstocc(vector<int> arr, int target,int n){
-    int s=0,e=n-1;
-    int mid=0,ans=-1;
-    while(s<=e){
-        mid=s+(e-s)/2;
-        if(arr[mid]==target)
-        {
-            ans=mid;
-            e=mid-1;
-        }
-        else if(arr[mid]>target)
-            e=mid-1;
-        
-        else if( target>arr[mid])
-        s=mid+1;
-    }
-    return ans;
-}
-int lastocc(vector<int> arr, int target){
-    int s=0,e=arr.size()-1;
-    int mid=0,ans=-1;
-    while(s<=e){
-        mid=s+(e-s)/2;
-        if(arr[mid]==target)
-        {
-            ans=mid;
-            s=mid+1;
-        }
-        else if(arr[mid]<target){
-            s=mid+1;
-        }
-        else if(arr[mid]>target)
-        e=mid-1;
-    }
-    return ans;
-}
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        int k=firstocc(nums,target,nums.size());
-        ans.push_back(k);
-        ans.push_back(lastocc(nums,target));
-        return ans;
+        int s=0,e=nums.size(),mid=0;
+        int first=-1,last=-1;
+        while(s<e){
+            mid=s+(e-s)/2;
+            cout<<mid;
+            if(nums[mid]==target){
+                first=mid;
+                last=mid;
+                while(first>0&&nums[first-1]==target)
+                    first--;
+                
+                while(last<nums.size()-1&&nums[last+1]==target)
+                    last++;
+                    break;
+            }
+            else if(nums[mid]>target)
+                e=mid;
+            else 
+                s=mid+1;
+        }
+        return {first,last};
     }
 };
