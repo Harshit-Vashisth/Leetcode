b9bdcb1d91085dd1e775eefd6674baeccea184cc
Harshit Vashisth <harshitvashisth76@gmail.com>
2026-01-17 16:19:35 +0530

    Time: 11 ms (55.32%), Space: 21.3 MB (23%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -10,14 +10,14 @@ public:
         return count;
     }
     int countSubstrings(string s) {
-        int count=0;
-        for(int i =0;i<s.length();i++){
+        int count =0;
+        for(int i=0;i<s.length();i++){
             //odd
-            int odd=expand(s, i,i);
-            //even
-            int even=expand(s, i,i+1);
-            count+=odd;
-            count+=even;
+           int odd=expand(s,i,i);
+           //even
+           int even=expand(s,i,i+1);
+           count+=odd;
+           count+=even;
         }
         return count;
     }
