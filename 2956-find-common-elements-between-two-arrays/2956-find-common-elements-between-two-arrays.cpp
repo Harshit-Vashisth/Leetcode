d1de2371598b9475360c7ee352c9688a02e41ded
Harshit Vashisth <harshitvashisth76@gmail.com>
2026-06-26 03:09:51 +0530

    Time: 7 ms (76.92%), Space: 64.8 MB (89.47%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int> mp1;
-        map<int,int> mp2;
-    for(int i=0;i<nums1.size();i++){
-        mp1[nums1[i]]++;
-    }
-    for(int i=0;i<nums2.size();i++){
-        mp2[nums2[i]]++;
-    }
-    int c1=0,c2=0;
-    for(int i=0;i<nums1.size();i++){
-        if(mp2[nums1[i]]!=0)
-        c1++;
-    }
-    for(int i=0;i<nums2.size();i++){
-        if(mp1[nums2[i]]!=0)
-        c2++;
-    }
-    return {c1,c2};
+        int ans1=0,ans2=0;
+        for(int i =0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i]==nums2[j]){
+                    ans1++;
+                     break;
+                }
+            }
+        }
+        for(int i =0;i<nums2.size();i++){
+            for(int j=0;j<nums1.size();j++){
+                if(nums2[i]==nums1[j]){
+                    ans2++;
+                    break;
+               }
+            }
+        }
+        return {ans1,ans2};
     }
 };
