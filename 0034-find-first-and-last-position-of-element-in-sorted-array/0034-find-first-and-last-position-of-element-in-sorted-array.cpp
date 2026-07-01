3db32c4653b4a974614d28748254c6ce6ce2e5d1
Harshit Vashisth <harshitvashisth76@gmail.com>
2026-07-01 13:39:07 +0530

    Time: 0 ms (100%), Space: 17.7 MB (19.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int s=0,e=nums.size(),mid=0;
-        int first=-1,last=-1;
-        while(s<e){
+    int first(vector<int>& nums,int target,int s,int e){
+        if(nums.size()==0)
+        return -1;
+        int ans=-1;
+        int mid=s+(e-s)/2;
+        while(s<=e){
+           
             mid=s+(e-s)/2;
-            cout<<mid;
             if(nums[mid]==target){
-                first=mid;
-                last=mid;
-                while(first>0&&nums[first-1]==target)
-                    first--;
-                
-                while(last<nums.size()-1&&nums[last+1]==target)
-                    last++;
-                    break;
-            }
-            else if(nums[mid]>target)
-                e=mid;
-            else 
-                s=mid+1;
+            e=mid-1;
+            ans=mid;}
+            else if(target>nums[mid])
+            s=mid+1;
+            else
+            e=mid-1;
         }
-        return {first,last};
+        return ans;
+    }
+    int last(vector<int>& nums,int target,int s,int e){
+        if(nums.size()==0)
+        return -1;
+        int mid=s+(e-s)/2;
+        int ans=-1;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(nums[mid]==target){
+            s=mid+1;
+            ans=mid;}
+            else if(target>nums[mid])
+            s=mid+1;
+            else
+            e=mid-1;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        ans.push_back(first(nums,target,0,nums.size()-1));
+        ans.push_back(last(nums,target,0,nums.size()-1));
+        return ans;
     }
 };
