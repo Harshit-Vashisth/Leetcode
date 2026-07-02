d4aeabab4d96f558f20b8995c6ea1f6ab1c8f273
Harshit Vashisth <harshitvashisth76@gmail.com>
2026-07-03 02:06:52 +0530

    Time: 0 ms (100%), Space: 15.2 MB (34.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,47 +1,53 @@
 class Solution {
 public:
-    int peak(vector<int>& arr) {
-        int e=arr.size()-1;
-        int s=0;
-        int mid;
+    int peek(vector<int>& nums){
+        int s=0,e=nums.size()-1,mid=s+(e-s)/2;
         while(s<=e){
             mid=s+(e-s)/2;
-            if(mid-1>=0&&arr[mid]<arr[mid-1]){
-                return mid-1;
+            if((mid-1>=0)&&nums[mid]<nums[mid-1])
+            return mid-1;
+            if(mid+1<nums.size()&&nums[mid]>nums[mid+1])
+             return mid;
+            if(nums[s]>=nums[mid]){
+                e=mid-1;
             }
-            if(mid+1<arr.size()&&arr[mid]>arr[mid+1]){
-                return mid;
+            else{
+                s=mid+1;
             }
-            if(arr[s]>=arr[mid])
-            e=mid-1;
-            else s=mid+1;
         }
-          return -1;
+        return -1;
     }
-    int binarysearch(vector<int> arr, int s, int e,int target){
-        int mid=0;
+    int search(vector<int>& nums, int s, int e,int target){
+        int mid=s+(e-s)/2;
+        cout<<s<<e<<endl;
         while(s<=e){
             mid=s+(e-s)/2;
-            if(arr[mid]==target)
+            if(nums[mid]==target)
             return mid;
-            else if(arr[mid]<target)
-            s=mid+1;
+            else if (nums[mid]<target){
+                s=mid+1;
+            }
             else 
             e=mid-1;
+            
         }
         return -1;
     }
     int search(vector<int>& nums, int target) {
-        int p=peak(nums);
-        cout<<p;
+        
+        int p=peek(nums);
+        cout<<p<<endl;
         if(target==nums[0])
         return 0;
         if(p==-1)
-         return binarysearch(nums,0,nums.size()-1,target);
-   
-        if(target>=nums[0]&&target<=nums[p])
-        return binarysearch(nums,0,p,target);
-        else
-        return binarysearch(nums,p+1,nums.size()-1,target);
+        return  search(nums,0,nums.size()-1,target);
+        if(target<=nums[p]&&target>=nums[0]){
+            return search(nums,0,p,target);
+        }
+        else {
+            //right
+            return search(nums,p+1,nums.size()-1,target);
+        }
+        return -1;
     }
 };
